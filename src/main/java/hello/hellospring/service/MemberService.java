3f4093b95e5b88c6d78c89fb22362c4ab30153ea
Taewon Yoon <Tonyzorz@naver.com>
2021-12-23 14:13:47 +0900

    AOP 추가 완료

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -31,19 +31,11 @@ public class MemberService {
         result.ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다");
         });*/
-        long start = System.currentTimeMillis();
-        try {
 
-            //코드의 가독성을 위해
-            validateDuplicateMember(member);
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join  = " + timeMs + "ms");
-        }
-
+        //코드의 가독성을 위해
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
